e637a66ccf523f8dff2ef4d6e6dce84225751612
Jeff Chao <jchao@netflix.com>
2019-07-03 16:08:27 -0400

    Ensure npm shrinkwrap packages config.
    When we do `oclif-dev pack`, the config dir should get bundled with the
    cli binary.

--- src/lib/aws/config.js
+++ src/lib/aws/config.js
@@ -16,22 +16,22 @@ module.exports = {
   keyPair: 'mantis',
   zookeeper: {
     securityGroup: 'zookeeper',
-    cloudInitTemplate: path.join(__dirname, '/../../../config/aws/cloud-init-templates/zookeeper-cloud-init.sh'),
+    cloudInitTemplate: path.join(__dirname, '/../../config/aws/cloud-init-templates/zookeeper-cloud-init.sh'),
   },
   mesosMaster: {
     securityGroup: 'mesos-master',
-    cloudInitTemplate: path.join(__dirname, '/../../../config/aws/cloud-init-templates/mesos-master-cloud-init.sh'),
+    cloudInitTemplate: path.join(__dirname, '/../../config/aws/cloud-init-templates/mesos-master-cloud-init.sh'),
   },
   mesosSlave: {
     securityGroup: 'mesos-slave',
-    cloudInitTemplate: path.join(__dirname, '/../../../config/aws/cloud-init-templates/mantis-agent-cloud-init.sh'),
+    cloudInitTemplate: path.join(__dirname, '/../../config/aws/cloud-init-templates/mantis-agent-cloud-init.sh'),
   },
   mantisControlPlane: {
     securityGroup: 'mantis-control-plane',
-    cloudInitTemplate: path.join(__dirname, '/../../../config/aws/cloud-init-templates/mantis-control-plane-cloud-init.sh'),
+    cloudInitTemplate: path.join(__dirname, '/../../config/aws/cloud-init-templates/mantis-control-plane-cloud-init.sh'),
   },
   mantisApi: {
     securityGroup: 'mantis-api',
-    cloudInitTemplate: path.join(__dirname, '/../../../config/aws/cloud-init-templates/mantis-api-cloud-init.sh'),
+    cloudInitTemplate: path.join(__dirname, '/../../config/aws/cloud-init-templates/mantis-api-cloud-init.sh'),
   },
 }
